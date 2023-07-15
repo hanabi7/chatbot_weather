079827380503ef786d639ae7e5ea0b51c80ede70
liuguanze <liuguanze@softsugar.com>
2023-07-15 08:48:02 +0800

    add date query

--- Action/bot.py
+++ Action/bot.py
@@ -8,8 +8,23 @@ from requests import (
     TooManyRedirects,
     Timeout
 )
+import ipdb
+from duckling import Duckling 
+from duckling.language import Language
 from api import get_weather_by_day
 
+duckling_engine = Duckling()
+duckling_engine.load()
+
+def compute_relative_time(data_context):
+    current_data_config = duckling_engine.parse('now')
+    input_data_config = duckling_engine.parse(data_context, language=Language.CHINESE)
+    current_data_value = current_data_config[-1]['value']['value']
+    input_data_value = input_data_config[-1]['value']['value']
+    current_date = current_data_value.split('T')[0].split('-')[-1]
+    input_date = input_data_value.split('T')[0].split('-')[-1]
+    relative_time = int(input_date) - int(current_date)
+    return relative_time
 
 class ActionReportWeather(FormAction):
     RANDOMIZE = True
@@ -29,9 +44,7 @@ class ActionReportWeather(FormAction):
         # type: (Dispatcher, DialogueStateTracker, Domain) -> List[Event]
         address = tracker.get_slot('address')
         date_time = tracker.get_slot('date-time')
-
         date_time_number = text_date_to_number_date(date_time)
-
         if isinstance(date_time_number, str):  # parse date_time failed
             return [SlotSet("matches", "暂不支持查询 {} 的天气".format([address, date_time_number]))]
         else:
@@ -64,16 +77,24 @@ def get_text_weather_date(address, date_time, date_time_number):
 def text_date_to_number_date(text_date):
     if text_date == "今天":
         return 0
-    if text_date == "明天":
+    elif text_date == "明天":
         return 1
-    if text_date == "后天":
+    elif text_date == "后天":
         return 2
 
     # Not supported by weather API provider freely
-    if text_date == "大后天":
+    elif text_date == "大后天":
         # return 3
         return text_date
-
+    elif text_date == "昨天":
+        return text_date
+    elif text_date == "前天":
+        return text_date
+    elif text_date == "大前天":
+        return text_date
+    else:
+        return compute_relative_time(text_date)
+    """
     if text_date.startswith("星期"):
         # TODO: using calender to compute relative date
         return text_date
@@ -83,9 +104,4 @@ def text_date_to_number_date(text_date):
         return text_date
 
     # follow APIs are not supported by weather API provider freely
-    if text_date == "昨天":
-        return text_date
-    if text_date == "前天":
-        return text_date
-    if text_date == "大前天":
-        return text_date
+    """
